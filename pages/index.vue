8b1cfbe663fb7cc22ee207875eeee8c9ff30dc9a
Max <scorpiomax90@gmail.com>
2024-01-31 22:19:25 +0200

    SEO and Meta

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,14 @@
 		Hello from the main page
 	</div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Main page',
+  ogTitle: 'Main Page',
+  description: 'This is the main page',
+  ogDescription: 'This is my amazing site, let me tell you all about it.',
+  ogImage: 'https://example.com/image.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
