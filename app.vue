b4bcb3640d9a2f8a81edd42afa6759261e448728
Max <scorpiomax90@gmail.com>
2024-02-01 22:01:58 +0200

    add fonts

--- app.vue
+++ app.vue
@@ -4,6 +4,24 @@
 	</NuxtLayout>
 </template>
 
-<!-- <script setup>
-const layout = 'another'
-</script> -->
+<script setup>
+useHead({
+  link: [
+    {
+      rel: 'preconnect',
+      href: 'https://fonts.googleapis.com'
+    },
+    {
+      rel: 'stylesheet',
+      href: 'https://fonts.googleapis.com/css2?family=Roboto&display=swap',
+      crossorigin: ''
+    }
+  ]
+})
+</script>
+
+<style>
+body {
+	font-family: 'Roboto', sans-serif;
+}
+</style>
