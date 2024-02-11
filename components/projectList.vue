74eaa643fc918e6e665e0a029124a03f2e7a189e
Max <scorpiomax90@gmail.com>
2024-02-11 21:49:58 +0200

    Listing projects in github

--- components/projectList.vue
+++ components/projectList.vue
@@ -1,8 +1,21 @@
 <template>
-	<div class="mb-2 text-xl py-2 text-red-800">Projects</div>
+	<p class="mb-2 text-xl py-2 text-red-800">Take a look at my GitHub projects!</p>
+
 	<section v-if="pending">Loading...</section>
-	<section v-else-if="error">Wrong... Try again!</section>
-	<section v-else>Here we display the repos!</section>
+	<section v-else-if="error">Error... Try again!</section>
+	<section v-else>
+		<ul class="grid grid-cols-1 gap-4">
+			<li
+				v-for="repository in data"
+				:key="repository.id"
+				class="border border-gray-200 rounded-sm p-4 hover:bg-gray-100 font-mono"
+			>
+				<a :href="repository.html_url" target="_blank">
+					<div class="font-semibold"> {{ repository.name }} </div>
+				</a>
+			</li>
+		</ul>
+	</section>
 </template>
 
 <script setup>
