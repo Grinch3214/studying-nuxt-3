d6af05c13648728a03121171d64f94b4ebb4673c
Max <scorpiomax90@gmail.com>
2024-03-10 22:43:18 +0200

    theme customization

--- app.vue
+++ app.vue
@@ -23,5 +23,18 @@ useHead({
 <style>
 body {
 	font-family: 'Roboto', sans-serif;
+	@apply dark:bg-gray-900 bg-white text-gray-700 dark:text-gray-300;
 }
+/* body {
+  background-color: #fff;
+  color: rgba(0,0,0,0.8);
+}
+.dark body {
+  background-color: #091a28;
+  color: #ebf4f1;
+}
+.sepia body {
+  background-color: #f1e7d0;
+  color: #433422;
+} */
 </style>
