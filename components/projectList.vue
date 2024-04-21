cfd71b7e7d5a4bb7195099b4f550c3721db20b42
Max <scorpiomax90@gmail.com>
2024-04-21 22:26:27 +0300

    fixed dark mode color for projects page

--- components/projectList.vue
+++ components/projectList.vue
@@ -1,25 +1,29 @@
 <template>
-	<p class="mb-2 text-xl py-2 text-red-800">GitHub projects!</p>
+  <p class="mb-2 text-xl py-2 text-red-800">GitHub projects!</p>
 
-	<section v-if="pending">Loading...</section>
-	<section v-else-if="error">Error... Try again!</section>
-	<section v-else>
-		<p class="text-right mb-3 text-blue-800 font-mono">repos: {{ data.length }}</p>
-		<ul class="grid grid-cols-1 gap-4">
-			<li
-				v-for="repository in data"
-				:key="repository.id"
-				class="border border-gray-200 hover:bg-gray-100 font-mono overflow-hidden rounded-md"
-			>
-				<a :href="repository.html_url" target="_blank" class="block p-4">
-					<div class="font-semibold"> {{ repository.name }} </div>
-					<div>{{ repository.description }}</div>
-				</a>
-			</li>
-		</ul>
-	</section>
+  <section v-if="pending">Loading...</section>
+  <section v-else-if="error">Error... Try again!</section>
+  <section v-else>
+    <p class="text-right mb-3 text-blue-800 font-mono">
+      repos: {{ data.length }}
+    </p>
+    <ul class="grid grid-cols-1 gap-4">
+      <li
+        v-for="repository in data"
+        :key="repository.id"
+        class="border border-gray-200 hover:bg-gray-100 dark:hover:bg-gray-700 dark:hover:text-white font-mono overflow-hidden rounded-md"
+      >
+        <a :href="repository.html_url" target="_blank" class="block p-4">
+          <div class="font-semibold">{{ repository.name }}</div>
+          <div>{{ repository.description }}</div>
+        </a>
+      </li>
+    </ul>
+  </section>
 </template>
 
 <script setup>
-const { error, pending, data } = await useFetch('https://api.github.com/users/Grinch3214/repos?per_page=100&page=1')
+const { error, pending, data } = await useFetch(
+  "https://api.github.com/users/Grinch3214/repos?per_page=100&page=1"
+);
 </script>
