b77a7304d9690f4a71fb98913167d2a4c30b06a0
Max <scorpiomax90@gmail.com>
2024-06-05 15:44:11 +0300

    changes styles in proj-list

--- components/projectList.vue
+++ components/projectList.vue
@@ -4,7 +4,7 @@
   <section v-if="pending">Loading...</section>
   <section v-else-if="error">Error... Try again!</section>
   <section v-else>
-    <p class="text-right mb-3 text-blue-800 font-mono">
+    <p class="text-right mb-2 text-blue-700 font-mono">
       repos: {{ data.length }}
     </p>
     <ul class="grid grid-cols-1 gap-4">
