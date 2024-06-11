b378b6eef46ed4a68f209f77b3922bbd4ae6b70d
Max <scorpiomax90@gmail.com>
2024-06-11 23:45:21 +0300

    change styles in default component

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,20 @@
 <template>
-	<div class="container mx-auto max-w-2xl">
-		<header class="flex justify-between items-center mt-5">
-			<div class="flex items-center gap-4 w-full">
-				<div>
-					<NuxtLink to="/" class="text-xl font-semibold p-2 hover:text-gray-300">Max</NuxtLink>
-				</div>
-				<Menu class="ml-auto" />
-				<ColorModeSelector />
-			</div>
-		</header>
-		<main class="p-2 mt-3">
-			<slot />
-		</main>
-	</div>
+  <div class="container mx-auto max-w-2xl">
+    <header class="flex justify-between items-center mt-5">
+      <div class="flex items-center gap-4 w-full">
+        <div>
+          <NuxtLink
+            to="/"
+            class="text-xl font-semibold py-2 px-3 hover:text-gray-400"
+            >Max</NuxtLink
+          >
+        </div>
+        <Menu class="ml-auto" />
+        <ColorModeSelector />
+      </div>
+    </header>
+    <main class="p-2 mt-3">
+      <slot />
+    </main>
+  </div>
 </template>
