a18de352211167353c93a6e76b44f851af001857
Max <scorpiomax90@gmail.com>
2024-06-18 21:59:34 +0300

    fixed text color in link

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,7 @@
         <div>
           <NuxtLink
             to="/"
-            class="text-xl font-semibold py-2 px-3 hover:text-gray-400 text-[#505020]"
+            class="text-xl font-semibold py-2 px-3 hover:text-gray-400"
             >Max</NuxtLink
           >
         </div>
