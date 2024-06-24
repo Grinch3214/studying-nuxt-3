50d59d387fe92b312f784db75fa9cfffeae4cac3
Max <scorpiomax90@gmail.com>
2024-06-24 23:38:44 +0300

    fixed menu items

--- components/menu.vue
+++ components/menu.vue
@@ -1,21 +1,19 @@
 <template>
   <nav>
     <ul class="flex space-x-4 items-center">
-      <li>
-        <NuxtLink to="/" class="link">Main</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/about" class="link">About</NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/projects" class="link">Projects</NuxtLink>
+      <li v-for="link in links" :key="link.name">
+        <NuxtLink :to="link.path" class="hover:text-zinc-400">{{
+          link.name
+        }}</NuxtLink>
       </li>
     </ul>
   </nav>
 </template>
 
-<style scoped>
-.link {
-  @apply hover:text-gray-500;
-}
-</style>
+<script setup>
+const links = [
+  { name: "Main", path: "/" },
+  { name: "About", path: "/about" },
+  { name: "Projects", path: "/projects" },
+];
+</script>
