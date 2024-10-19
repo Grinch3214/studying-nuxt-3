26a7096d86bceb45670570cb469c047693c6e6c0
Max <scorpiomax90@gmail.com>
2024-10-19 11:20:06 +0300

    watch test page

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,14 @@
 <template>
-	<div>
-		Hello from the main page
-	</div>
+  <div>Hello from the main page (test)</div>
 </template>
 
 <script setup>
 useSeoMeta({
-  title: 'Main page',
-  ogTitle: 'Main Page',
-  description: 'This is the main page',
-  ogDescription: 'This is my amazing site, let me tell you all about it.',
-  ogImage: 'https://example.com/image.png',
-  twitterCard: 'summary_large_image',
-})
+  title: "Main page",
+  ogTitle: "Main Page",
+  description: "This is the main page",
+  ogDescription: "This is my amazing site, let me tell you all about it.",
+  ogImage: "https://example.com/image.png",
+  twitterCard: "summary_large_image",
+});
 </script>
