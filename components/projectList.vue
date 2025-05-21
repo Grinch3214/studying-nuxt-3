1ce94a3c0a5af2d2566a90d548914161fda3cdaf
Max <scorpiomax90@gmail.com>
2025-05-21 23:38:30 +0300

    fix gap

--- components/projectList.vue
+++ components/projectList.vue
@@ -7,7 +7,7 @@
     <p class="text-right mb-2 text-blue-800 font-mono">
       Repos: {{ data.length }}
     </p>
-    <ul class="grid grid-cols-1 gap-4">
+    <ul class="grid grid-cols-1 gap-2">
       <li
         v-for="repository in data"
         :key="repository.id"
