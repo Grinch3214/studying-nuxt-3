aef3a38fae91915ea2a2f15ba5bd1c21b37dab3c
Max <scorpiomax90@gmail.com>
2025-07-03 09:20:30 +0300

    fix default

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,13 +2,9 @@
   <div class="container mx-auto max-w-[1200px]">
     <header class="flex justify-between items-center mt-5">
       <div class="flex items-center gap-4 w-full">
-        <div>
-          <NuxtLink
-            to="/"
-            class="text-xl font-bold py-2 px-4 hover:text-gray-300"
-            >Max</NuxtLink
-          >
-        </div>
+        <NuxtLink to="/" class="text-xl font-bold py-2 px-4 hover:text-gray-300"
+          >Max</NuxtLink
+        >
         <Menu class="ml-auto my-3" />
         <ColorModeSelector />
       </div>
