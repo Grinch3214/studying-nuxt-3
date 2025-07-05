66d7239e9db06a8b27e89fc571de6f4b3cc3b927
Max <scorpiomax90@gmail.com>
2025-07-05 09:21:42 +0300

    fix spacing

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,14 +2,16 @@
   <div class="container mx-auto max-w-[1200px]">
     <header class="flex justify-between items-center mt-5">
       <div class="flex items-center gap-4 w-full">
-        <NuxtLink to="/" class="text-xl font-bold py-2 px-4 hover:text-gray-300"
+        <NuxtLink
+          to="/"
+          class="text-2xl font-medium py-2 px-4 hover:text-gray-300"
           >Max</NuxtLink
         >
         <Menu class="ml-auto my-3" />
         <ColorModeSelector />
       </div>
     </header>
-    <main class="p-2 mt-3">
+    <main class="p-2 mt-2">
       <slot />
     </main>
   </div>
