cddcc525136ad81448c169e09acce95ac5844cc6
Max <scorpiomax90@gmail.com>
2025-07-16 23:09:17 +0300

    added ts

--- components/menu.vue
+++ components/menu.vue
@@ -25,5 +25,5 @@ const links = [
   { id: 2, name: "About", path: "/about" },
   { id: 3, name: "Projects", path: "/projects" },
   { id: 4, name: "Blog", path: "/blog" },
-];
+] as Link[];
 </script>
