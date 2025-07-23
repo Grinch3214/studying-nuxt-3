f11ccd3c5c8ad3df0829eccf728fedb667cf8c4f
Max <scorpiomax90@gmail.com>
2025-07-23 23:29:23 +0300

    added req prop

--- components/projectList.vue
+++ components/projectList.vue
@@ -13,7 +13,7 @@
         :key="repository.id"
         class="border border-gray-500 hover:bg-gray-200 dark:hover:bg-gray-600 dark:hover:text-white font-mono overflow-hidden rounded-md"
       >
-        <projectItem />
+        <projectItem :repository="repository" />
       </li>
     </ul>
   </section>
