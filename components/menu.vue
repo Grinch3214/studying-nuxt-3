0d0fdbcebf430fb084ee5d11f6f22ce663911b91
Max <scorpiomax90@gmail.com>
2025-10-28 08:51:21 +0200

    Update hover color

--- components/menu.vue
+++ components/menu.vue
@@ -4,7 +4,7 @@
       <li v-for="link in links" :key="link.id">
         <NuxtLink
           :to="link.path"
-          class="hover:text-zinc-200 transition-opacity"
+          class="hover:text-zinc-300 transition-opacity"
         >
           {{ link.name }}
         </NuxtLink>
@@ -21,9 +21,9 @@ interface Link {
 }
 
 const links = [
-  { id: 1, name: "Main", path: "/" },
-  { id: 2, name: "About", path: "/about" },
-  { id: 3, name: "Projects", path: "/projects" },
-  { id: 4, name: "Blog", path: "/blog" },
+  { id: 1, name: 'Main', path: '/' },
+  { id: 2, name: 'About', path: '/about' },
+  { id: 3, name: 'Projects', path: '/projects' },
+  { id: 4, name: 'Blog', path: '/blog' },
 ] as Link[];
 </script>
