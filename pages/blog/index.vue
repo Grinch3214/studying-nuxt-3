907e4ed000d29172ec5a35f5ff920b975deecfd6
Max <scorpiomax90@gmail.com>
2026-01-12 00:37:00 +0200

    Added style for blog

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,8 +1,8 @@
 <template>
   <h2>All posts here</h2>
-  <div v-for="item in data">{{ item.title }}</div>
+  <div class="grid gap-3" v-for="item in data">{{ item.title }}</div>
 </template>
 
 <script setup>
-const { data } = await useAsyncData("blog", () => queryContent("/").find());
+const { data } = await useAsyncData('blog', () => queryContent('/').find());
 </script>
